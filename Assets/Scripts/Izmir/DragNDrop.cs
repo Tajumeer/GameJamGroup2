308bde74aebec65d4cc31adbb8f7e9d772fc1d8c
Outchase <izmir.rexhepi27@hotmail.com>
2023-05-18 22:56:35 +0200

    drag fixes

--- Assets/Scripts/Izmir/DragNDrop.cs
+++ Assets/Scripts/Izmir/DragNDrop.cs
@@ -8,6 +8,12 @@ public class DragNDrop : MonoBehaviour
     [SerializeField] bool isDragable;
     [SerializeField] float dragSpeed = 10;
 
+    private Collider2D _collider;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
     private Vector3 GetMouseWorldPostion()
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -31,6 +37,19 @@ public class DragNDrop : MonoBehaviour
         if (isDragable)
         {
             transform.position = Vector3.MoveTowards(transform.position, GetMouseWorldPostion() + mousePositionOffset, dragSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DragNDrop collidedDraggable = GetComponent<DragNDrop>();
+
+        if (collidedDraggable != null)
+        {
+            ColliderDistance2D colliderDistance2D = other.Distance(_collider);
+
+            Vector3 diff = new Vector3(colliderDistance2D.normal.x, colliderDistance2D.normal.y) * colliderDistance2D.distance;
+            transform.position -= diff;
         }
     }
 }

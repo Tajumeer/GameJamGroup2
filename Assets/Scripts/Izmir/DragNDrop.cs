4e8d27a803b35f477a772d47e6329a2a3e80e53a
Outchase <izmir.rexhepi27@hotmail.com>
2023-05-19 12:40:57 +0200

    fixed

--- Assets/Scripts/Izmir/DragNDrop.cs
+++ Assets/Scripts/Izmir/DragNDrop.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DragNDrop : MonoBehaviour
 {
     Vector3 mousePositionOffset;
-    [SerializeField] bool isDragable;
     [SerializeField] float dragSpeed = 10;
 
     private Collider2D _collider;
@@ -21,23 +20,13 @@ public class DragNDrop : MonoBehaviour
 
     private void OnMouseDown()
     {
-
-        if (isDragable)
-        {
             mousePositionOffset = gameObject.transform.position - GetMouseWorldPostion();
-        }
-        else
-        {
-            Debug.Log("I got pressed");
-
-        }
+        
+        
     }
     private void OnMouseDrag()
     {
-        if (isDragable)
-        {
             transform.position = Vector3.MoveTowards(transform.position, GetMouseWorldPostion() + mousePositionOffset, dragSpeed * Time.deltaTime);
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
